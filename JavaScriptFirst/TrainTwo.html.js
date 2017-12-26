2e96cca3ca5b45904121664e4f74340bd7d4af51
jack <350922075@qq.com>
2017-12-26 11:28:49 +0800

    MyJquery

---
+++ JavaScriptFirst/TrainTwo.html.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TrainTwo</title>
+</head>
+<body>
+<script type="text/javascript">
+	var t=prompt("请输入一个整数");
+	if(t>5&&t<13){
+		for (var i = 0; t>i; i--) {
+			document.write("E:\Material\smile");
+		}
+	}
+</script>
+</body>
+</html>
